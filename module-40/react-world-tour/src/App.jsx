d0d68c7d3da814e6f89863b6a2e626a5d071f4b5
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-09-09 21:31:08 +0600

    40 start

---
+++ module-40/react-world-tour/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Country from './assets/Components/Countries/Countries'
+
+
+function App() {
+
+  return (
+    <>  
+      <h1>Vite + React</h1>
+      <Country></Country>
+    </>
+  )
+}
+
+export default App
