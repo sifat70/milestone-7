6b9baa1080c977712c99e505d4909d18a0490209
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-09-10 12:25:38 +0600

    40 done

--- module-40/react-world-tour/src/assets/Components/Countries/Countries.jsx
+++ module-40/react-world-tour/src/assets/Components/Countries/Countries.jsx
@@ -1,22 +1,64 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css'
 
 const Countries = () => {
 
     const [countries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    const [visitedFlags, setVisitedFlags] = useState([])
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
+
+
+    const handleVisitedCountry = country =>{
+        console.log('add the visited country')
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries)
+    }
+
+    const handleVisitedFlags = flag =>{
+        const newVisitedFlags = [...visitedFlags, flag];
+        setVisitedFlags(newVisitedFlags)
+    }
+
+
+    // remove item from an array  in a state
+    // use filter to select all the elements expect the one you want to remove
+
+
     return (
         <div>
             <h3>Countries : {countries.length}</h3>
-            {
-                countries.map(country=><Country key={country.cca3} country={country}></Country>)
-            }
+             
+            <div>
+                <h5>Visited Countries: {visitedCountries.length}</h5>
+                <ul>
+                    {
+                        visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+                    }
+                </ul>
+            </div>
+            <div className="flag-container">
+                {
+                    visitedFlags.map((flag, inx) => <img key={inx} src={flag}></img>)
+                }
+            </div>
+            {/* Display country */}
+            <div className="country-container">
+                {
+                    countries.map(country => <Country
+                        key={country.cca3}
+                        handleVisitedCountry = {handleVisitedCountry}
+                        handleVisitedFlags = {handleVisitedFlags}
+                        country={country}></Country>)
+                }
+            </div>
         </div>
     );
 };
---
+++ module-40/react-world-tour/src/assets/Components/CountryData/CountryData.jsx
@@ -0,0 +1,11 @@
+const CountryData = ({country, handleVisitedCountry, handleVisitedFlags}) => {
+
+    console.log('inside Country data', country, handleVisitedCountry, handleVisitedFlags)
+    return (
+        <div>
+            <p><small>Country Data: {country.name.common}</small></p>
+        </div>
+    );
+};
+
+export default CountryData;
